437f108476879f9dcb645f53e949d1ec8169fb20
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2024-09-08 13:17:11 +0300

    Add SerializedObjectExtensions

---
+++ Tequila/Scripts/Editor/SerializedObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace IL.Tequila
+{
+    public static class SerializedObjectExtensions
+    {
+        public static IEnumerable<SerializedProperty> GetSerializedProperties(this SerializedObject serializedObject, bool enterChildren, bool onlyVisible)
+        {
+            using var iterator = serializedObject.GetIterator();
+
+            if (onlyVisible)
+            {
+                while (iterator.NextVisible(enterChildren))
+                {
+                    yield return iterator;
+                }
+            }
+            else
+            {
+                while (iterator.Next(enterChildren))
+                {
+                    yield return iterator;
+                }
+            }
+        }
+    }
+}
