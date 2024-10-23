bbe2b81af9befedb6cbfb4f9c29c973a292e5024
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2024-10-24 00:05:03 +0300

    Update CustomAttributeProviderExtensions

--- Tequila/Scripts/Runtime/CustomAttributeProviderExtensions.cs
+++ Tequila/Scripts/Runtime/CustomAttributeProviderExtensions.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace IL.Tequila
 {
     public static class CustomAttributeProviderExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsDefined<TAttribute>(this ICustomAttributeProvider provider, bool inherit)
             where TAttribute : Attribute
         {
