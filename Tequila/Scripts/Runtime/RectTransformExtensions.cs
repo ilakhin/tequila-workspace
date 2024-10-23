39b4ac021113f0c3b09c7e83ad7db4d36cb9bd1a
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2024-10-24 00:04:42 +0300

    Add RectTransformExtensions

---
+++ Tequila/Scripts/Runtime/RectTransformExtensions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace IL.Tequila
+{
+    public static class RectTransformExtensions
+    {
+        public static Rect GetWorldRect(this RectTransform rectTransform)
+        {
+            var rect = rectTransform.rect;
+            var matrix = rectTransform.localToWorldMatrix;
+
+            rect.min = matrix.MultiplyPoint(rect.min);
+            rect.max = matrix.MultiplyPoint(rect.max);
+
+            return rect;
+        }
+    }
+}
